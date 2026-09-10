3452902a59e857d1dba389e3067cdb0b4ee0abae
maulanialmufidailmi-sys <maulanialmufidailmi@gmail.com>
2026-09-10 15:19:49 +0700

    Add files via upload

---
+++ PEKAN 1/Tugas1_2611531006.py
@@ -0,0 +1,12 @@
+print("               BIODATA MAHASISWA                  ")
+print("==================================================")
+print("Nama Lengkap    : Maulani Almufida Ilmi")
+print("NIM             : 2611531006")
+print("Fakultas        : Teknologi Informasi")
+print("Departemen      : Informatika")
+print("Universitas     : Universitas Andalas")
+print("Tahun Angkatan  : 2026")
+print("Mata Kuliah     : Praktikum Algoritma & Pemrograman")
+
+print("Status Program  : Berhasil Dijalankan")
+print("==================================================")
