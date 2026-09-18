10ac025200bbefa65bad63d41484ab01acdad544
maulanialmufidailmi-sys <maulanialmufidailmi@gmail.com>
2026-09-18 18:20:51 +0700

    Rename variables and update print statements

--- Pekan3/Perbandingan_2611531006.py
+++ Pekan3/Perbandingan_2611531006.py
@@ -1,42 +1,42 @@
 # Program operator perbandingan dalam python
 
-angka1 = int(input("Input angka-1:"))
-angka2 = int(input("Input angka-2:"))
+angka1_1006 = int(input("Input angka-1: "))
+angka2_1006 = int(input("Input angka-2: "))
 
 # Lebih besar dari
-hasil = angka1 > angka2
-print("\nOperator Lebih Besar Dari")
-print("angka1 > angka2 =", hasil)
+hasil_1006 = angka1_1006 > angka2_1006
+print("\nOperator lebih besar dari")
+print("angka1 > angka2 =",hasil_1006)
 
 # Lebih kecil dari
-hasil = angka1 < angka2
-print("\nOperator Lebih Kecil Dari")
-print("angka1 < angka2 =", hasil)
+hasil_1006 = angka1_1006 < angka2_1006
+print("\nOperator lebih kecil dari")
+print("angka1 < angka2 =",hasil_1006)
 
 # Lebih besar dari atau sama dengan
-hasil = angka1 >= angka2
-print("\nOperator Lebih Besar Dari atau Sama Dengan")
-print("angka1 >= angka2 =", hasil)
+hasil_1006 = angka1_1006 >= angka2_1006
+print("\nOperator lebih besar dari atau sama dengan")
+print("angka1 >= angka2 =",hasil_1006)
 
 # Lebih kecil dari atau sama dengan
-hasil = angka1 <= angka2
-print("\nOperator Lebih Kecil Dari atau Sama Dengan")
-print("angka1 <= angka2 =", hasil)
+hasil_1006 = angka1_1006 <= angka2_1006
+print("\nOperator lebih kecil dari atau sama dengan")
+print("angka1 <= angka2 =",hasil_1006)
 
 # Sama dengan
-hasil = angka1 == angka2
-print("\nOperator Sama Dengan")
-print("angka1 == angka2 =", hasil)
+hasil_1006 = angka1_1006 == angka2_1006
+print("\nOperator sama dengan")
+print("angka1 == angka2 =",hasil_1006)
 
 # Tidak sama dengan
-hasil = angka1 != angka2
-print("\nOperator Tidak Sama Dengan")
-print("angka1 != angka2 =", hasil)
+hasil_1006 = angka1_1006 != angka2_1006
+print("\nOperator tidak sama dengan")
+print("angka1 != angka2 =",hasil_1006)
 
-# Tambahan: perbandingan berantai dalam python
-hasil = 0 < angka1 < 100
-print("\nOperator Perbandingan Berantai")
-print("0 < angka1 < 100 =", hasil)
+# Tambahan: perbandingan berantai dalam Python
+hasil_1006 = 0 < angka1_1006 < 100
+print("\nPerbandingan berantai")
+print("0 < angka1_1006 < 100 =",hasil_1006)
 
-hasil = 0 < angka2 < 100
-print("0 < angka2 < 100 =", hasil)
+hasil_1006 = 0 < angka2_1006 < 100
+print("0 < angka2_1006 < 100 =",hasil_1006)
