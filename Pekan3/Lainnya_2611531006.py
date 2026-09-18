afc336195761e513bf4cb7307c84a157a93034e3
maulanialmufidailmi-sys <maulanialmufidailmi@gmail.com>
2026-09-18 18:18:47 +0700

    Refactor membership and identity operators example

--- Pekan3/Lainnya_2611531006.py
+++ Pekan3/Lainnya_2611531006.py
@@ -1,54 +1,55 @@
 # Program keanggotaan dan identitas dalam python
 
-print("==================================")
-print("1.OPERATOR KEANGGOTAAN")
-print("==================================")
+print("======================================")
+print("1. OPERATOR KEANGGOTAAN")
+print("======================================")
 
-# Input beberapa data yang dipisahkan dengan koma'
-Input_data = input("Input beberapa data, pisahkan dengan koma:")
+# Input beberapa data yang dipisahkan dengan koma
+input_data_1006 = input("Masukkan beberapa angka, pisahkan dengan koma: ")
 
 # Mengubah input menjadi list integer
-data = [int(angka.strip()) for angka in Input_data.split(",")]
+data_1006 = [int(angka.strip()) for angka in input_data_1006.split(',')]
 
-nilai_dicari = int(input("Masukkan angka yang ingin dicari:"))
+nilai_dicari_1006 = int(input("Masukkan angka yang ingin dicari: "))
 
 # Operator in
-hasil = nilai_dicari in data
+hasil_1006 = nilai_dicari_1006 in data_1006
 print("\nOperator keanggotaan IN")
-print(nilai_dicari, "in data =", hasil)
+print(nilai_dicari_1006,"in",data_1006,"=",hasil_1006)
 
-#Operator not in
-hasil = nilai_dicari not in data
+# Operator not in
+hasil_1006 = nilai_dicari_1006 not in data_1006
 print("\nOperator keanggotaan NOT IN")
-print(nilai_dicari, "not in", data, "=", hasil)
-print("==================================")
-print("2.OPERATOR IDENTITAS")
-print("==================================")
+print(nilai_dicari_1006,"not in",data_1006,"=",hasil_1006)
 
-# objek1 menggunakan list dari input pengguna
-objek1 = data
+print("======================================")
+print("2. OPERATOR IDENTIAS")
+print("======================================")
+
+# objek1 menggunakan list dari input perngguna
+object1_1006 = data_1006
 
 # objek2 merujuk pada objek yang sama dengan objek1
-objek2 = objek1
+object2_1006 = object1_1006
 
 # objek3 memiliki isi sama, tetapi merupakan objek baru
-objek3 = data.copy()
+object3_1006 = data_1006.copy()
 
-print("objek1 =", objek1)
-print("objek2 =", objek2)
-print("objek3 =", objek3)
+print("object1 =",object1_1006)
+print("object2 =",object2_1006)
+print("object3 =",object3_1006)
 
 # Operator is
-hasil = objek1 is objek2
+hasil_1006 = object1_1006 is object2_1006
 print("\nOperator identitas IS")
-print("objek1 is objek2 =", hasil)
+print("objek1 is objek2 =",hasil_1006)
 
 # Operator is not
-hasil = objek1 is not objek3
+hasil_1006 = object1_1006 is not object2_1006
 print("\nOperator identitas IS NOT")
-print("objek1 is not objek3 =", hasil)
+print("objek1 is not objek2 =",hasil_1006)
 
 # Membandingkan identitas dan nilai
 print("\nPerbandingan identitas dan nilai")
-print("objek1 is objek3 =", objek1 is objek3)
-print("objek1 == objek3 =", objek1 == objek3)
+print("objek1 is objek3 =",object1_1006 is object3_1006)
+print("objek1 == objek3 =",object1_1006 == object3_1006)
